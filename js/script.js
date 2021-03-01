754a66e4da832911209c8671fdb2151109a9e07b
FrancescaLai <francescalai1690@gmail.com>
2021-03-01 15:02:33 +0100

    Ticket price calculation (no discount added yet)

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ var km = parseInt(prompt("Inserisci i km da percorrere"));
 var eta = parseInt(prompt("Inserisci la tua età"));
 
 var prezzoKm = 0.2;
+
+var prezzoBiglietto = prezzoKm * km;
+
+document.getElementById('prezzo').innerHTML = "Il prezzo del tuo biglietto è di €" + prezzoBiglietto;
