72aacfcb6e0a7cac8d0aad069c7058edb532f235
FrancescaLai <francescalai1690@gmail.com>
2021-03-01 15:12:30 +0100

    discount added

--- js/script.js
+++ js/script.js
@@ -6,4 +6,19 @@ var prezzoKm = 0.2;
 
 var prezzoBiglietto = prezzoKm * km;
 
+// Se anni < 18, 20% discount
+
+// Se anni >=65, 40% discount
+
+if (eta < 18){
+  var sconto = prezzoBiglietto * 0.2;
+  prezzoBiglietto = prezzoBiglietto - sconto;
+  console.log(sconto.toFixed(2));
+} else if (eta >= 65 ) {
+  var sconto = prezzoBiglietto * 0.4;
+  prezzoBiglietto = prezzoBiglietto - sconto;
+  console.log(sconto.toFixed(2));
+}
+
+
 document.getElementById('prezzo').innerHTML = "Il prezzo del tuo biglietto è di €" + prezzoBiglietto;
